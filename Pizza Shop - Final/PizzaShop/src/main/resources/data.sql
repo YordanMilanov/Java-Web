40468038721deeaeca685ba574be9b82ec255d8d
YordanMilanov <dancho.galinov@gmail.com>
2023-03-31 22:03:19 +0300

    initializing the users done

--- Pizza Shop - Final/PizzaShop/src/main/resources/data.sql
+++ Pizza Shop - Final/PizzaShop/src/main/resources/data.sql
@@ -1,18 +1,34 @@
+-- Addresses
+INSERT INTO addresses (id,city, neighborhood, street, street_number)
+VALUES(1,'City','Center','street', 1);
+
+INSERT INTO addresses (id, city, neighborhood, street, street_number)
+VALUES(2,'City','Center','street', 2);
+
+INSERT INTO addresses (id, city, neighborhood, street, street_number)
+VALUES(3,'City','Center','street', 3);
+
+INSERT INTO addresses (id, city, neighborhood, street, street_number)
+VALUES(4,'City','Center','street', 4);
+
+INSERT INTO addresses (id, city, neighborhood, street, street_number)
+VALUES(5,'City','Center','street', 5);
+
 -- some test users
-INSERT INTO users (id, username, password, full_name, email, level)
-VALUES (1, 'manager', 'manager', 'mr.Manager', 'manager@abv.bg', 'EMPLOYEE');
+INSERT INTO users (id, username, password, full_name, email, level, `address_id`)
+VALUES (1, 'manager', 'manager', 'mr.Manager', 'manager@abv.bg', 'EMPLOYEE', 1);
 
-INSERT INTO users (id, username, password, full_name, email, level)
-VALUES (2,'staff', 'staff', 'mr.Staff', 'staff@abv.bg', 'EMPLOYEE');
+INSERT INTO users (id, username, password, full_name, email, level, `address_id`)
+VALUES (2,'staff', 'staff', 'mr.Staff', 'staff@abv.bg', 'EMPLOYEE', 2);
 
-INSERT INTO users (id, username, password, full_name, email, level)
-VALUES (3,'customer-new', 'customer-new', 'mr.Customer-new', 'customer-new@abv.bg', 'CUSTOMER');
+INSERT INTO users (id, username, password, full_name, email, level, `address_id`)
+VALUES (3,'customer-new', 'customer-new', 'mr.Customer-new', 'customer-new@abv.bg', 'CUSTOMER', 3);
 
-INSERT INTO users (id, username, password, full_name, email, level)
-VALUES (4,'customer-regular', 'customer-regular', 'mr.Customer-regular', 'customer-regular@abv.bg', 'CUSTOMER');
+INSERT INTO users (id, username, password, full_name, email, level, `address_id`)
+VALUES (4,'customer-regular', 'customer-regular', 'mr.Customer-regular', 'customer-regular@abv.bg', 'CUSTOMER', 4);
 
-INSERT INTO users (id, username, password, full_name, email, level)
-VALUES (5,'customer-vip', 'customer-vip', 'mr.Customer-vip', 'customer-vip@abv.bg', 'CUSTOMER');
+INSERT INTO users (id, username, password, full_name, email, level, `address_id`)
+VALUES (5,'customer-vip', 'customer-vip', 'mr.Customer-vip', 'customer-vip@abv.bg', 'CUSTOMER', 5);
 
 -- user roles
 
@@ -41,6 +57,3 @@ VALUES (4, 3);
 -- Customer - vip
 INSERT INTO users_roles (`user_id`, `roles_id`)
 VALUES (5, 3);
-
-
-
